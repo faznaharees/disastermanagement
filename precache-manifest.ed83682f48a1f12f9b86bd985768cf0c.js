7d6b9b6f7af4368ac4d08a7a307c1f01e25997a4
Fazna <faznaharees@cet.ac.in>
2020-07-16 11:43:12 +0530

    Updates

---
+++ precache-manifest.ed83682f48a1f12f9b86bd985768cf0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca0d69f0c9df68fa29d80e660f8671f",
+    "url": "/disastermanagement/index.html"
+  },
+  {
+    "revision": "609c88007757b91b3909",
+    "url": "/disastermanagement/static/css/main.32426efc.chunk.css"
+  },
+  {
+    "revision": "77f01b742ab5f927a5eb",
+    "url": "/disastermanagement/static/js/2.07f6c1da.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/disastermanagement/static/js/2.07f6c1da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "609c88007757b91b3909",
+    "url": "/disastermanagement/static/js/main.3580683e.chunk.js"
+  },
+  {
+    "revision": "d3655457e6025d10703c",
+    "url": "/disastermanagement/static/js/runtime-main.92d3f31c.js"
+  }
+]);
